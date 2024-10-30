459c98923e9f9de9021f25f852c6f5b85d11a045
polyhistor2050 <chrisspaul2050@gmail.com>
2024-10-30 19:35:04 +0300

    Fetch data from the api and pick random word to display

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [solutions, setSolutions] = useState("");
+  // Fetch data on mounts
+
+  useEffect(() => {
+    const fetchWord = async () => {
+      const response = await fetch("/wordle-words");
+      const words = await response.json();
+      const rand = Math.floor(Math.random() * words.length);
+      const randomWord = words[rand];
+      setSolutions(randomWord);
+    };
+
+    fetchWord();
+  }, []);
   return (
     <>
-      <h1>Hello, World!</h1>
+      <h1>RANDOM WORD: {solutions}</h1>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 4000,
+    proxy: {
+      "/wordle-words": {
+        target: "https://api.frontendexpert.io",
+        changeOrigin: true,
+        rewrite: (path) =>
+          path.replace(/^\/wordle-words/, "/api/fe/wordle-words"),
+      },
+    },
   },
 });
